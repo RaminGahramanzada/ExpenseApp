73b762b6bd7fe824e1bd510c63d5fc1dcbda65cf
ramin <r.gahramanzada@andersenlab.com>
2026-03-16 16:26:25 +0400

    First initialization

---
+++ ExpenseAppV2/src/main/java/org/example/expenseappv2/ExpenseAppV2Application.java
@@ -0,0 +1,13 @@
+package org.example.expenseappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseAppV2Application.class, args);
+    }
+
+}
